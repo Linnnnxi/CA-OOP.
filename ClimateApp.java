2620ae9e27e0318e1bc169df0d67ada0b09bc9c9
Linnnnxi <x23177152@student.ncirl.ie>
2024-11-14 20:55:54 +0000

    Linru second commit

---
+++ ClimateApp.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.climateapp;
+
+/**
+ *
+ * @author Linru Wang
+ */
+public class ClimateApp {
+
+    public static void main(String[] args) {
+        ClimateGUI myGUI= new ClimateGUI();
+        myGUI.setVisible(true);
+    }
+}
